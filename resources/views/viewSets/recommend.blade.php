60a0126aad4099b6088a249e5de700a37769df79
taku matsunaga <skyline199306@yahoo.co.jp>
2021-03-19 19:27:53 +0900

    create recommend coord

--- resources/views/viewSets/recommend.blade.php
+++ resources/views/viewSets/recommend.blade.php
@@ -1,7 +1,46 @@
 @section('mainBody')
 
-<div>
-    test
-</div>
+<section>
+    <div class="headerBlank"></div>
+
+    <div class="textContainer">
+        <div class="recommendTitle">
+            <h2><span>おすすめのコーディネート by wearcoord</span></h2>
+        </div>
+        <div class="recommendTags">
+            <div class="tags backColorPink">#春</div>
+            <div class="tags backColorGray">#nike</div>
+        </div>
+    </div>
+
+    <ul class="reccomendImgContainer">
+        <a href=""><li class="reccomendImg_item">
+            <img src="{{asset('img/testImg/ナイキ-スポーツウェア-ダウンフィル-ウィンドランナー-シールド-メンズベスト-CPp1sd.jpg')}}" alt="">
+        </li></a>
+        <a href=""><li class="reccomendImg_item">
+            <img src="{{asset('img/testImg/ナイキ-スポーツウェア-ダウンフィル-ウィンドランナー-シールド-メンズベスト-CPp1sd.jpg')}}" alt="">
+        </li></a>
+        <a href=""><li class="reccomendImg_item">
+            <img src="{{asset('img/testImg/ナイキ-スポーツウェア-ダウンフィル-ウィンドランナー-シールド-メンズベスト-CPp1sd.jpg')}}" alt="">
+        </li></a>
+        <a href=""><li class="reccomendImg_item">
+            <img src="{{asset('img/testImg/ナイキ-スポーツウェア-ダウンフィル-ウィンドランナー-シールド-メンズベスト-CPp1sd.jpg')}}" alt="">
+        </li></a>
+        <a href=""><li class="reccomendImg_item">
+            <img src="{{asset('img/testImg/ナイキ-スポーツウェア-ダウンフィル-ウィンドランナー-シールド-メンズベスト-CPp1sd.jpg')}}" alt="">
+        </li></a>
+            <div class="reccomendImg_item moreIcon">
+                <div class="moreIcon_inner">
+                    <a href="" class="moreIcon_text">
+                        <p>MORE</p>
+                        <span class="material-icons-outlined iconArrow">
+                        arrow_forward_ios
+                        </span></div>
+                    </a>
+
+                </div>
+    </ul>
+
+</section>
 
 @endsection
