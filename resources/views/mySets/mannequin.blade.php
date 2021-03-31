3718481b1a690cd72718d3991c935a480f2f0a39
taku matsunaga <skyline199306@yahoo.co.jp>
2021-03-31 18:38:52 +0900

    fix mannequin blade

---
+++ resources/views/mySets/mannequin.blade.php
@@ -0,0 +1,53 @@
+<div class="mannequinImg">
+
+    {{-- caps --}}
+    <form action="{{ route('searchmysetsGetCaps') }}" class="capsForm" method="get">
+        @csrf
+        <button class="mannequinBtn"  type="submit">
+            <input type="hidden" name="type" value="caps">
+            <img class="mannequinItemImg" src="{{ asset('/img/testImg/la-foresta_10084789.png') }}" alt="">
+        </button>
+    </form>
+
+    {{-- face --}}
+    <div class="faceDiv"></div>
+
+    {{-- tops --}}
+    <form action="{{ route('searchmysetsGetTops') }}" class="topsForm" method="get">
+        @csrf
+        <button class="mannequinBtn"  type="submit">
+            <input type="hidden" name="type" value="caps">
+            @if ((asset( $arrayUrl['topsUrl'] )) != asset(''))
+            <img class="mannequinItemImg" src="{{ asset( $arrayUrl['topsUrl'] ) }}" alt="{{ asset( $arrayUrl['topsUrl'] ) }}">
+            @endif
+        </button>
+    </form>
+
+    {{-- pants --}}
+    <form action="{{ route('searchmysetsGetPants') }}" class="pantsForm" method="get">
+        @csrf
+        <button class="mannequinBtn"  type="submit">
+            <input type="hidden" name="type" value="caps">
+            @if ((asset( $arrayUrl['pantsUrl'] )) != asset(''))
+            {{-- <img class="mannequinItemImg" src="{{ asset( $arrayUrl['pantsUrl'] ) }}" alt=""> --}}
+            <img class="mannequinItemImg" src="{{ asset( '/img/testImg/0331/la-foresta_10085297.png' ) }}" alt="">
+            @endif
+        </button>
+    </form>
+
+    {{-- socks --}}
+    <div class="socksImg">
+        <div class="socksBox" style="background-color: red"></div>
+        <div class="socksBox" style="background-color: red"></div>
+    </div>
+
+    {{-- shoes --}}
+    <form action="{{ route('searchmysetsGetShoes') }}" class="shoesForm" method="get">
+        @csrf
+        <button class="mannequinBtn"  type="submit">
+            <input type="hidden" name="type" value="caps">
+            <img class="mannequinItemImg" src="{{ asset('/img/testImg/alpen_10353285.png') }}" alt="">
+        </button>
+    </form>
+
+</div>
