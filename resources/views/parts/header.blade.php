667a22715696db3f99efa76447b218b58db44e00
taku matsunaga <skyline199306@yahoo.co.jp>
2021-04-07 14:03:05 +0900

    add route dashboard

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -5,7 +5,7 @@
     <a href="{{route('home')}}" class="titleIcon">
         <h1 class="mainTitle">wearcoord</h1>
     </a>
-    <a class="settingIcon" href="">
+    <a class="settingIcon" href="{{route('dashboard')}}">
         <span class="material-icons-outlined">settings</span>
     </a>
 </header>
