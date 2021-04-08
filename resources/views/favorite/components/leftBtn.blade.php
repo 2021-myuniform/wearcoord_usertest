b12ef8c4d327bd5a6578dd7d75b188562aaf0ee9
taku matsunaga <skyline199306@yahoo.co.jp>
2021-04-08 16:51:41 +0900

    fix inport inner

--- resources/views/favorite/components/leftBtn.blade.php
+++ resources/views/favorite/components/leftBtn.blade.php
@@ -34,19 +34,24 @@
                 <p class="btnText" id="btnTitleInner">Inner</p>
         </summary>
         <div class="detailsBottom">
-            <a href="" class="detailsBtn">
+            {{-- <a href="" class="detailsBtn">
                 <span class="material-icons-outlined">
                     pageview
                     </span>
                 <p class="btnText">見る</p>
             </a>
-                <hr>
-            <a href="" class="detailsBtn">
-                <span class="material-icons-outlined">
-                    file_download
-                    </span>
-                <p class="btnText">アイテムを反映</p>
-            </a>
+                <hr> --}}
+            <form action="{{ route('importItem') }}"  class="detailsBtn" method="post">
+                @csrf
+                <button type="submit">
+                    <span class="material-icons-outlined">
+                        file_download
+                        </span>
+                    <p class="btnText">アイテムを反映</p>
+                    <input type="hidden" name="type" value="inner">
+                    <input type="hidden" name="favid" value="{{$favid}}">
+                </button>
+            </form>
         </div>
     </details>
 </div>
