f55db9631acd0bbf11d3e63e713e654db74ce920
taku matsunaga <skyline199306@yahoo.co.jp>
2021-04-28 18:00:30 +0900

    create lp

---
+++ public/js/lp.js
@@ -0,0 +1,17 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    autoplay: {
+      delay: 0,
+    },
+    loop: true,
+    speed: 10000,
+    disableOnInteraction: false,
+  });
+
+var mySwiper = new Swiper ('.pants-container', {
+    autoplay: {
+      delay: 0,
+      reverseDirection: true,
+    },
+    loop: true,
+    speed: 10000,
+  });
